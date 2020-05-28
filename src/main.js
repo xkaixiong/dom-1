3890c4fab8b195272cb162eccf9c1936bdf16031
kaixiong <258976882@qq.com>
2020-05-28 22:26:38 +0800

    dom-1

---
+++ src/main.js
@@ -0,0 +1,47 @@
+// const div = dom.create("<div>newDiv</div>");
+// console.log(div);
+
+// dom.after(test, div);
+
+// const div3 = dom.create('<div id="parent"></div>');
+// dom.wrap(test, div3)
+
+// const nodes = dom.empty(window.empty)
+// console.log(nodes)
+
+// dom.attr(test, 'title', 'Hi,I am kx')
+// const title = dom.attr(test, 'title')
+// console.log(`title:${title}`)
+
+// dom.text(test, '你好，这是新的内容')
+
+// dom.style(test, { border: '1px solid red', color: 'blue' })
+// console.log(dom.style(test, 'border'))
+// dom.style(test, 'border', '1px solid black')
+// dom.style(test, 'color', 'red')
+
+// dom.class.add(test, 'red')
+// dom.class.add(test, 'bule')
+// dom.class.remove(test, 'bule')
+// console.log(dom.class.has(test, 'blue'))
+
+// const fn = () => {
+//     console.log('点击了')
+// }
+// dom.on(test, 'click', fn)
+// dom.off(test, 'click', fn)
+
+// const testDiv = dom.find('#test')[0]
+// const test2 = dom.find('#test2')[0]
+// console.log(dom.find('.red', test2)[0])
+
+// console.log(dom.parent(test))
+
+// console.log(dom.siblings(dom.find('#s2')[0]))
+
+// console.log(dom.next(dom.find('#s2')[0]))
+
+// const t = dom.find('#travel')[0]
+// dom.each(dom.children(t), (n) => dom.style(n, 'color', 'red'))
+
+// console.log(dom.index(s2))
